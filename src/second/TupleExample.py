bec4729ace104c4ea0dbb6e5eb452d193c8d298e
Shudipto <Shudiptotrafder@gmail.com>
2018-01-01 20:34:25 +0600

    tuple

--- src/second/TupleExample.py
+++ src/second/TupleExample.py
@@ -4,3 +4,27 @@ print("Tuple:", tp)
 print(type(tp))
 
 print("Index: 3", tp[3])
+
+# convert tuple to list
+
+li = list(tp)
+print("\nList: ", li)
+
+print("\n\n")
+# tuple pack
+tp = 0, 1, 2, 3
+print(tp)
+
+print("\nUnpack tuple")
+a, b, c, d = tp
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("d:", d)
+
+# Create tuple with single element
+t = (1)
+print(type(t))
+
+t = (1, )
+print(type(t))
