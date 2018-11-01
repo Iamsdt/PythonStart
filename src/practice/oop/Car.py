5fa8d85cd8ac4f9d784833d1d1b2dc2b3d9a918a
Shudipto <Shudiptotrafder@gmail.com>
2018-11-01 23:26:32 +0600

    oop

---
+++ src/practice/oop/Car.py
@@ -0,0 +1,21 @@
+class Car:
+    _fuel = 0.0
+
+    _rate = 0.2
+
+    def __init__(self, name, year, cc, fuel):
+        self.name = name
+        self.year = year
+        self.cc = cc
+        self._fuel += fuel
+
+    def add_fuel(self, amount):
+        self._fuel += amount
+
+    def run(self, time):
+        temp = time * self._rate
+        self._fuel -= temp
+
+    def fuelStatus(self):
+        print("Current Fuel: ", self._fuel)
+
---
+++ src/practice/oop/TestCar.py
@@ -0,0 +1,10 @@
+import src.practice.oop.Car as Car
+
+car = Car.Car("Tata", 2018, 300, 20)
+car.fuelStatus()
+
+print("Run car")
+car.run(12)
+
+car.fuelStatus()
+
